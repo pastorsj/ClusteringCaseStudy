2a2c29c3167fae443cd62e4fe1b69810714f8609
pastorsj <sampastoriza@yahoo.com>
2021-10-03 10:03:42 -0400

    Update clustering code

--- ElbowMethod/main.py
+++ ElbowMethod/main.py
@@ -11,8 +11,8 @@ import math
 clustered_x_values = np.array([3, 1, 1, 2, 1, 6, 6, 6, 5, 6, 7, 8, 9, 8, 9, 9, 8])
 clustered_y_values = np.array([5, 4, 5, 6, 5, 8, 6, 7, 6, 7, 1, 2, 1, 2, 3, 2, 3])
 
-unclustered_x_values = np.array([math.floor(random.random() * 10) for _ in range(100)])
-unclustered_y_values = np.array([math.floor(random.random() * 10) for _ in range(100)])
+unclustered_x_values = np.array([random.random() * 10 for _ in range(1000)])
+unclustered_y_values = np.array([random.random() * 10 for _ in range(1000)])
 
 # List of K-Clusters
 k_range = range(1, 10)
@@ -28,11 +28,13 @@ def run_elbow_method_using_distortion(x_values, y_values):
     for k in k_range:
         k_means = KMeans(n_clusters=k)
         k_means.fit(points)
+
+        # Calculate distortion per k value
         centroids = k_means.cluster_centers_
-        # Naive approach, this gives all of the distances, then we need to find the min values
         distances_from_centroids = cdist(points, centroids, 'euclidean')
         smallest_distances_from_centroids = np.min(distances_from_centroids, axis=1)
         calculated_distortion = sum(smallest_distances_from_centroids) / points.shape[0]
+
         list_of_distortions.append(calculated_distortion)
 
     plot_elbow_method(k_range, list_of_distortions, 'Distortion', 'Plot of elbow method using Distortion')
@@ -44,7 +46,10 @@ def run_elbow_method_using_inertia(x_values, y_values):
     for k in k_range:
         k_means = KMeans(n_clusters=k)
         k_means.fit(points)
+
+        # Calculate inertia per k value
         calculated_inertia = k_means.inertia_  # Sum of the squared distances to the nearest cluster
+
         list_of_inertias.append(calculated_inertia)
 
     plot_elbow_method(k_range, list_of_inertias, 'Inertia', 'Plot of elbow method using Inertia')
